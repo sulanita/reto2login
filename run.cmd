93d0e076ca73b5b628d596ec8841a35c7850b196
Pedro Rozo <pedro.rozo@gmail.com>
2023-04-09 20:54:12 -0500

    Ajustes

---
+++ run.cmd
@@ -0,0 +1,1 @@
+java -DAPPLICATIONINSIGHTS_CONNECTION_STRING="InstrumentationKey=29225906-1a2a-4e98-877e-f5b8354bb833;IngestionEndpoint=https://eastus2-3.in.applicationinsights.azure.com/;LiveEndpoint=https://eastus2.livediagnostics.monitor.azure.com/" -javaagent:"d:\app\applicationinsights-agent-3.4.11.jar" -jar target/login-1.0.jar
